87a65d4a7cd5cdabaf7c707574f7d7893591f90a
Mikhail Mingareev <mingareev@myheat.net>
2025-04-22 17:43:34 +0300

    add column to users

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -41,6 +41,7 @@ class BotController extends Controller
                 User::create([
                     'name' => $request->all()["message"]["chat"]["first_name"],
                     'email' => $request->all()["message"]["chat"]["id"] . '@mail.ru',
+                    'password' => '123456`',
                     'tg_username' => $request->all()["message"]["chat"]["username"],
                     'is_bot' => $request->all()["message"]["from"]["is_bot"],
                     'language_code' => $request->all()["message"]["from"]["language_code"],
